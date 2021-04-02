57383db2e93f857cc8dc169f7b7ee034b61c11bd
AnteAntonini <anteantonini.mtk@hotmail.com>
2021-04-02 11:17:53 +0200

    task solved

---
+++ src/components/Master.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h1>Master </h1>
+    </div>
+</template>
+
+
+
+<script>
+import Gateway from '@nsoft/seven-gravity-gateway/master';
+
+
+let master = Gateway({
+    slaves : {
+         GatewayPlayground: {
+                frameId : 'frameExample',
+                data : {
+                    test: "hello from master",
+                    mess: 'another message'
+                },
+                init: function() {
+                    console.log('initialized master')
+                },
+         }
+    },
+    allowedOrigins : '*',
+    debug : false,
+})
+
+master.on("HelloMaster", (event) => console.log(event.data.message));
+
+master.emit('frameExample', {
+    action : 'Betslip.Add',
+    data: {test: 'test123'},
+    slaveId : 'GatewayPlayground'
+}, '*')
+
+
+export default {
+    name: 'Master',
+}
+
+</script>
---
+++ src/components/Slave.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <iframe id="frameExample"
+    title="Frame Example"
+    width="500"
+    height="500"
+    src="https://seven-integration-example.7platform.net/">
+</iframe>
+
+  </div>
+</template>
+
+
+<script>
+import Gateway from '@nsoft/seven-gravity-gateway/slave';
+
+
+let slave = Gateway({
+    slaveId : 'GatewayPlayground',
+    data : {test: "hello from slave"},
+    allowedOrigins: '*',
+    debug : false
+});
+
+slave.emit({
+  action : 'Betslip.Add',
+}, '*');
+
+
+export default {
+  name: 'Slave',
+  data() {
+    return {
+    
+    }
+  }
+
+}
+
+</script>
+
+<style>
+.hello {
+  display: flex;
+  justify-content: center;
+  margin-top: 40px;
+  align-content: center;
+  align-items: center;
+}
+</style>
+
